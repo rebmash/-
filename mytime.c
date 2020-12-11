a02fe4f7483ce1f7ca7c9ed93ba79bcde074fd91
rebmash <72229322+rebmash@users.noreply.github.com>
2020-12-11 14:59:48 +0300

    Add files via upload

--- mytime.c
+++ mytime.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <sys/times.h>
 int main(int argc, char* argv[])
 { 
   struct timespec tp1;
@@ -22,7 +22,15 @@ int main(int argc, char* argv[])
     wait(0);
     clock_gettime(CLOCK_REALTIME, &tp2);
     long int time = ((tp2.tv_sec - tp1.tv_sec) * 1e9 + tp2.tv_nsec - tp1.tv_nsec);
-    printf("%ld.%ldms\n", time/1000000, time%1000000);
+    struct tms buf;
+    times(&buf);
+    long int a = sysconf(_SC_CLK_TCK);
+    //for (int i = 1; i < argc; i++)
+   // {
+    //    printf("%s ", argv[i]);
+    //}
+    printf("real    %ld.%lds\n", time/1000000000, time%1000000000);
+    printf("user    %ld,00s\n", (buf.tms_utime + buf.tms_cutime) * a );
+    printf("sys     %ld,00s\n", (buf.tms_stime + buf.tms_cstime) * a);
   }
-  
 }
