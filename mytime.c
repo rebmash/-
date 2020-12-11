40a94e2db5a28ac5690adc1fe19f1a209ac4c718
rebmash <72229322+rebmash@users.noreply.github.com>
2020-12-11 15:39:07 +0300

    Add files via upload

--- mytime.c
+++ mytime.c
@@ -7,40 +7,38 @@
 #include <sys/times.h>
 #include <pthread.h>
 #include <errno.h>
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 
-  struct timespec tp1;
-  struct timespec tp2;
-  clock_gettime(CLOCK_REALTIME, &tp1);
-  int flag = 0;
-  int k = fork();
-  if (k == 0)
-  {
-  
-    int a = execvp(argv[1], argv + 1);
-    if (a == -1)
-    {
-        perror("zsh");
+    struct timespec tp1;
+    struct timespec tp2;
+    clock_gettime(CLOCK_REALTIME, &tp1);
+
+
+    int k = fork();
+    if (k == 0) {
+
+        int a = execvp(argv[1], argv + 1);
+        if (a == -1) {
+            perror("bash");
+        }
+
+        exit(0);
+    } else {
+        wait(0);
+
+        clock_gettime(CLOCK_REALTIME, &tp2);
+        double time = ((tp2.tv_sec - tp1.tv_sec) + (tp2.tv_nsec - tp1.tv_nsec) * 1e-9);
+        struct tms buf;
+        times(&buf);
+        long int a = sysconf(_SC_CLK_TCK); //число тиков в секунду
+        //for (int i = 1; i < argc; i++)
+        // {
+        //    printf("%s ", argv[i]);
+        //}
+        printf("real    %fs\n", time);
+        float t11 = (buf.tms_utime + buf.tms_cutime) / a;
+        printf("user    %fs\n", t11);
+        float t22 = (buf.tms_stime + buf.tms_cstime) / a;
+        printf("sys     %fs\n", t22);
     }
-
-    exit(0);
-  }
-  else
-  {
-    wait(0);
-
-    clock_gettime(CLOCK_REALTIME, &tp2);
-    long int time = ((tp2.tv_sec - tp1.tv_sec) * 1e9 + tp2.tv_nsec - tp1.tv_nsec);
-    struct tms buf;
-    times(&buf);
-    long int a = sysconf(_SC_CLK_TCK);
-    //for (int i = 1; i < argc; i++)
-   // {
-    //    printf("%s ", argv[i]);
-    //}
-    printf("real    %ld,%lds\n", time/1000000000, time%1000000000);
-    printf("user    %ld,00s\n", (buf.tms_utime + buf.tms_cutime) * a );
-    printf("sys     %ld,00s\n", (buf.tms_stime + buf.tms_cstime) * a);
-  }
 }
