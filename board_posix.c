3614f6e4f99553ef62d99d8c9c45fba8a917f01a
rebmash <72229322+rebmash@users.noreply.github.com>
2020-12-18 16:37:41 +0300

    Add files via upload

--- board_posix.c
+++ board_posix.c
@@ -16,13 +16,13 @@ int passangers;
 int ramp;
 int board;
 int voyages;
-sem_t *sem0, *sem1, *sem2, *sem3, *sem4, *sem5;
+sem_t *sem0, *sem1, *sem2, *sem3, *sem4, *sem5, *sem6;
 //sem0 - amount of passengers who got to queue
 //sem1 - amount of passengers who got on ramp
 //sem2 - amount of passengers who got on the bord
 //sem3 - voyage is started or not
 //sem4 -voyage is finished or not
-
+//sem5 - if it was the last voyage
 
 int passenger(int i)
 {
@@ -32,9 +32,16 @@ int passenger(int i)
     {
 
         sem_wait(sem0); //Passenger in queue
-        perror("sem_wait");
-        printf("Pas %d: I'm in the queue\n", i);
 
+        int value;
+        sem_getvalue(sem5, &value);
+        //printf("%d\n", value);
+        if (value > 0)
+        {
+            break;
+        }
+        printf("Pas %d: I'm in the queue\n", i);
+        sync();
 
         sem_wait(sem1); //Passenger getting on trap
         printf("Pas %d: I'm on the trap\n", i);
@@ -71,7 +78,7 @@ int passenger(int i)
        // semop(id, &a, 1);
         sem_post(sem1);
 
-        
+
         printf("Pas %d: I left the trap, Bye!\n", i);
         sync();
         //a.sem_num = 2;
@@ -80,7 +87,9 @@ int passenger(int i)
         sem_post(sem2);
 
     }
-    printf("pass");
+    sem_post(sem6);
+    printf("pass\n");
+    sync();
     //finish
     return 0;
 }
@@ -93,12 +102,12 @@ int cap()
 
     printf("Cap: trap  down\n");
     sync();
-    for (int i = 0; i < voyages; i ++)
+    for (int j = 0; j < voyages; j++)
     {
 
        for (int i = 0; i < ramp; i++)
        {
-           perror("sem_post");
+           //perror("sem_post");
            sem_post(sem1);
        }
 
@@ -108,7 +117,7 @@ int cap()
         }
 
         printf("Cap: READYYYY FOR ANOTHER ONE\n");
-
+        sync();
 
 
         for (int i = 0; i < board; i++)
@@ -154,10 +163,19 @@ int cap()
         }
         printf("Cap: Finished! \n");
         sync();
+        if (j == voyages - 1)
+        {
+            sem_post(sem5);
+            for (int i = 0; i < passangers; i++)
+            {
+                sem_post(sem0); //пропускаем всех пассажиров в очередь
+            }
+        }
 
         //finish
     }
-    printf("kek");
+    sem_post(sem6);
+    printf("kek\n");
     sync();
     return 0;
 }
@@ -180,13 +198,21 @@ int min(int a, int b)
 int main(int ac, char *av[])
 {
     //id = semget(IPC_PRIVATE, 6, IPC_CREAT|0777);
+    sem_unlink("1");
+    sem_unlink("2");
+    sem_unlink("3");
+    sem_unlink("4");
+    sem_unlink("5");
+    sem_unlink("6");
+    sem_unlink("7");
     sem0 = sem_open("1", O_CREAT, 0644, 0);
     sem1 = sem_open("2", O_CREAT, 0644, 0);
     sem2 = sem_open("3", O_CREAT, 0644, 0);
     sem3 = sem_open("4", O_CREAT, 0644, 0);
     sem4 = sem_open("5", O_CREAT, 0644, 0);
     sem5 = sem_open("6", O_CREAT, 0644, 0);
-    perror("sem_open");
+    sem6 = sem_open("7", O_CREAT, 0644, 0);
+    //perror("sem_open");
     //sem_init(&sem1, 0, 0);
     //sem_init(&sem2, 0, 0);
     //sem_init(&sem3, 0, 0);
@@ -209,15 +235,8 @@ int main(int ac, char *av[])
     voyages = atoi(av[4]);
     board = min(passangers, board);
     int j;
-
-
-
-
-    j = fork();
-    if (j != 0)
-    {
-        int r = 1;
-        for (int i = 0; i < passangers; i++)
+    int r = 1;
+        for (int i = 0; i < passangers + 1; i++)
         {
             if (r != 0)
             {
@@ -225,29 +244,44 @@ int main(int ac, char *av[])
             }
             if (r == 0)
             {
-                passenger(i);
-                return(0);
+                if (i != passangers)
+                {
+                    passenger(i);
+                    exit(0);
+                }
+                else
+                {
+                    cap();
+                    exit(0);
+                }
             }
         }
 
-    }
-    else
+    if (r != 0)
     {
-        cap();
-        return(0);
+        for (int i = 0; i < passangers + 1; i++)
+        {
+            sem_wait(sem6);
+        }
+        int value;
+        sem_getvalue(sem0, &value);
+        printf("val0 - %d\n", value);
+        sem_getvalue(sem1, &value);
+        printf("val1 - %d\n", value);
+        sem_getvalue(sem2, &value);
+        printf("val2 - %d\n", value);
+        sem_getvalue(sem3, &value);
+        printf("val3 - %d\n", value);
+        sem_getvalue(sem4, &value);
+        printf("val4 - %d\n", value);
+        sem_getvalue(sem5, &value);
+        printf("val5 - %d\n", value);
+        sem_unlink("1");
+        sem_unlink("2");
+        sem_unlink("3");
+        sem_unlink("4");
+        sem_unlink("5");
+        sem_unlink("6");
+        sem_unlink("7");
     }
-    wait(0);
-    int value;
-    sem_getvalue(sem0, &value);
-    printf("val0 - %d\n", value);
-    sem_getvalue(sem1, &value);
-    printf("val1 - %d\n", value);
-    sem_getvalue(sem2, &value);
-    printf("val2 - %d\n", value);
-    sem_getvalue(sem3, &value);
-    printf("val3 - %d\n", value);
-    sem_getvalue(sem4, &value);
-    printf("val4 - %d\n", value);
-    sem_getvalue(sem5, &value);
-    printf("val5 - %d\n", value);
 }
